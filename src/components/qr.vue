a17c5619f35f5f20a5b790c7e0e07961bc4a10a6
cxp <fffg947221@qq.com>
2019-04-12 11:16:19 +0800

    添加qr插件

---
+++ src/components/qr.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :id="qrId">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'qr',
+    components: {},
+    data() {
+      return {
+        token: '',
+      }
+    },
+    props: {
+      qrMsg: {
+        type: String,
+        default: 'helloworld',
+      },
+      qrId: {
+        type: String,
+        required: true
+      },
+      width: {
+        type: Number,
+        default: 100,
+      },
+      height: {
+        type: Number,
+        default: 100,
+      },
+    },
+    created() {},
+    mounted() {
+      this.init();
+    },
+    methods: {
+      init() {
+        let qrcode = new QRCode(document.getElementById(this.qrId), {
+          text: this.qrMsg,
+          width: this.width,
+          height: this.height,
+          colorDark: "#000000",
+          colorLight: "#ffffff",
+          correctLevel: QRCode.CorrectLevel.H
+        });
+      },
+
+    },
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about pageabc</h1>
+    <qr :qrId="'adc'"></qr>
+    <echarts :echartID="'def'"></echarts>
+    <qr :qrId="'adcd'" qrMsg="i am hugh.chen" style="margin-top: 10px"></qr>
   </div>
 </template>
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
+import qr from '@/components/qr.vue'
+import echarts from '@/components/echarts.vue'
 
 export default {
   name: 'about',
   components: {
-    HelloWorld
+    HelloWorld,
+    qr,
+    echarts
   }
 }
 </script>
